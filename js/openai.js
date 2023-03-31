ebd33526639440c1b97eafca5ad581e374faef51
Andreas De Rosi <andreasderosi@Andreass-MacBook-Pro.local>
2023-03-31 16:29:20 +0530

    openai changes

--- js/openai.js
+++ js/openai.js
@@ -1,24 +1,29 @@
 async function sendToOpenAI(message) {
-    const apiKey = "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"; // Replace with your OpenAI API key
-    const apiURL = "https://api.openai.com/v1/engines/davinci-codex/completions";
-    const prompt = `Extract country and color from the following message: "${message}"`;
-  
-    const response = await fetch(apiURL, {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-        "Authorization": `Bearer ${apiKey}`,
-      },
-      body: JSON.stringify({
-        prompt: prompt,
-        max_tokens: 50,
-        n: 1,
-        stop: null,
-        temperature: 0.5,
-      }),
-    });
-  
-    const data = await response.json();
+  const apiKey = "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"; // Replace with your OpenAI API key
+  const apiURL = "https://api.openai.com/v1/engines/davinci-codex/completions";
+  const prompt = `Extract country and color from the following message: "${message}"`;
+
+  const response = await fetch(apiURL, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      "Authorization": `Bearer ${apiKey}`,
+    },
+    body: JSON.stringify({
+      prompt: prompt,
+      max_tokens: 50,
+      n: 1,
+      stop: null,
+      temperature: 0.5,
+    }),
+  });
+
+  const data = await response.json();
+  if (data.choices.length > 0) {
     const output = data.choices[0].text.trim();
     return output;
+  } else {
+    console.error("Empty response from OpenAI API");
+    return "";
   }
+}
