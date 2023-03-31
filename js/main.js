0602ac82135d5db07ce7e52f6db1d585bf5bb292
Andreas De Rosi <andreasderosi@Andreass-MacBook-Pro.local>
2023-03-31 16:21:39 +0530

    updated main js

--- js/main.js
+++ js/main.js
@@ -4,25 +4,42 @@ $(function() {
   var $chatboxMessages = $('.chatbox-messages');
   var $chatboxInput = $('.chatbox-input input[type="text"]');
   var $chatboxSendButton = $('.chatbox-input button');
-  
+
   // Function to append a message to the message history
   function appendMessage(message) {
     var $messageElement = $('<div>').text(message);
     $chatboxMessages.append($messageElement);
   }
-  
+
   // Function to handle sending a message
-  function sendMessage() {
+  async function sendMessage() {
     var message = $chatboxInput.val();
-    appendMessage(message);
+    appendMessage("You: " + message);
     $chatboxInput.val('');
+
+    // Send the message to OpenAI
+    var response = await sendToOpenAI(message);
+
+    // Extract the country and color from the OpenAI response
+    var countryColorRegex = /The country is (.+?) and the color is (.+?)\./;
+    var matches = response.match(countryColorRegex);
+
+    if (matches) {
+      var country = matches[1];
+      var color = matches[2];
+
+      // Create a map with Leaflet
+      createMap(country, color);
+    } else {
+      appendMessage("ChatMaps: I'm sorry, I couldn't extract the country and color from your message.");
+    }
   }
-  
+
   // Event handler for the send button click
   $chatboxSendButton.on('click', function() {
     sendMessage();
   });
-  
+
   // Event handler for the enter key press in the input field
   $chatboxInput.on('keypress', function(event) {
     if (event.which === 13) {
@@ -31,4 +48,3 @@ $(function() {
   });
 });
 
-  
