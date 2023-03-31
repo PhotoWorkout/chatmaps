e419fff421066bed1cc8afeb7edd290f73af624c
Andreas De Rosi <andreasderosi@Andreass-MacBook-Pro.local>
2023-03-31 16:38:30 +0530

    open ai engine changed

--- js/openai.js
+++ js/openai.js
@@ -1,6 +1,6 @@
 async function sendToOpenAI(message) {
   const apiKey = "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"; // Replace with your OpenAI API key
-  const apiURL = "https://api.openai.com/v1/engines/davinci-codex/completions";
+  const apiURL = "https://api.openai.com/v1/completions";
   const prompt = `Extract country and color from the following message: "${message}"`;
 
   const response = await fetch(apiURL, {
@@ -15,15 +15,12 @@ async function sendToOpenAI(message) {
       n: 1,
       stop: null,
       temperature: 0.5,
+      model: "davinci-codex"
     }),
   });
 
   const data = await response.json();
-  if (data.choices.length > 0) {
-    const output = data.choices[0].text.trim();
-    return output;
-  } else {
-    console.error("Empty response from OpenAI API");
-    return "";
-  }
+  const output = data.choices[0].text.trim();
+  return output;
 }
+
